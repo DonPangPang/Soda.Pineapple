e54a8d3684b58017c1a213e82786d7b3c6e14561
Powers <49866401+DonPangPang@users.noreply.github.com>
2023-07-17 17:03:39 +0800

    添加被忽略的文件

---
+++ src/Soda.Pineapple/Extensions/LazyResolutionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Soda.Pineapple.Extensions;
+
+internal static class LazyResolutionExtensions
+{
+    public static IServiceCollection AddLazyResolution(this IServiceCollection services)
+    {
+        return services.AddTransient(
+            typeof(Lazy<>),
+            typeof(LazilyResolved<>));
+    }
+}
+
+internal class LazilyResolved<T> : Lazy<T> where T : notnull
+{
+    public LazilyResolved(IServiceProvider serviceProvider)
+        : base(serviceProvider.GetRequiredService<T>)
+    {
+    }
+}
